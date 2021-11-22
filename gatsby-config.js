5ecddfe02c76dac4469d4ff7fa90317acbf6dae4
chewie-droid <56605946+chewie-droid@users.noreply.github.com>
2021-11-22 14:29:59 -0800

    edit to the site name and other metadata components

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Peter Chiu`,
+    siteTitleAlt: `Peter Chiu - web3 Starter Site`,
+    siteHeadline: `Peter Chiu - web3 Starter Site`,
+    siteUrl: `https://chiu.eth.link`,
+    siteDescription: `Tutorial website for web3 technologies`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@lekoarts_de did the theme`,
   },
   plugins: [
     {
